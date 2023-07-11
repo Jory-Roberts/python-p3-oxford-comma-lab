3e82e075ef4e31cefcee37bb68ffde1cbeac9c37
Jory Roberts <robertsjory13@gmail.com>
2023-07-11 12:50:38 -0500

    completes lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1 :
+        return " ".join(items)
+    elif len(items) == 2 :
+        return " and ".join(items)
+    else :
+        string_item = ""
+        i = 1
+        for item in items :
+            if(len(items) == i) :
+                string_item = string_item + "and " + item
+            else :
+                i += 1
+                string_item = string_item + item + ", "
+    return string_item
